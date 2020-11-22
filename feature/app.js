6b2eeb9bbb929a360ba20be386a03a801b7a81dc
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2020-11-22 20:38:34 +0100

    class hidden added and event for the class

--- feature/app.js
+++ feature/app.js
@@ -1,3 +1,19 @@
-const priceElement = document.getElementsByClassName('price');
+const priceElements = document.querySelectorAll('.price');
+
+
+priceElements.forEach(priceElement =>{
+    // nasłuchwanie na kliknęcie dla każdego elemntu
+    priceElement.addEventListener('click', (event) => {
+        console.log(event.currentTarget.children);
+
+    const children = event.currentTarget.children;
+
+    const hiddenParagraph = children[1];
+    // console.log(hiddenParagraph);
+
+    hiddenParagraph.classList.toggle('hidden')
+    })
+   
+}) 
 
 
