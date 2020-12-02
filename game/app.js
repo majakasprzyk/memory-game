cc1ef693a63bf33fab6b44dc53d45c16bec7e73d
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2020-12-02 22:56:07 +0100

    cards in random position

--- game/app.js
+++ game/app.js
@@ -10,7 +10,19 @@ const cardTypes = Array.from({length: 8})
         return `./img/${element}.jpg`;  // można to zapisać jeszecze tak: return "type-" + element;
     });
 
+// double the array 
+
 const cards = cardTypes.flatMap((type) => [type, type]);
+
+const cardsToPick = cards.map((item) => item);
+const cardsInRandomOrder = [];
+
+for (let i = 0; i <cards.length; i += 1){
+    const randomIndex = Math.floor(Math.random() * cardsToPick.length);
+    cardsInRandomOrder.push(cardsToPick.splice(randomIndex, 1)[0]);
+}
 
 console.log(cardTypes);
 console.log(cards);
+console.log(cardsToPick);
+console.log(cardsInRandomOrder);
