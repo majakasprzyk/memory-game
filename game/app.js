4ab6325b54344bb1a096c16fdaf055d6a855fe89
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2020-12-02 23:14:15 +0100

    img added to game

--- game/app.js
+++ game/app.js
@@ -7,7 +7,7 @@ const cardTypes = Array.from({length: 8})
     return index +1;
     })
     .map((element) => {
-        return `./img/${element}.jpg`;  // można to zapisać jeszecze tak: return "type-" + element;
+        return `./img/${element}.png`;  // można to zapisać jeszecze tak: return "type-" + element;
     });
 
 // double the array 
