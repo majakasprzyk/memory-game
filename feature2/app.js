f00517fc1a14b60701b8467e6d648b7e4287243a
Maja Kasprzyk <maja.kasprzyk@gmail.com>
2021-01-10 20:11:44 +0100

    WIP feature02

--- feature2/app.js
+++ feature2/app.js
@@ -1,19 +1,14 @@
-const priceElements = document.querySelectorAll('.price');
+var slideIndex = 0;
+carousel();
 
-
-priceElements.forEach(priceElement =>{
-    // nasłuchwanie na kliknęcie dla każdego elemntu
-    priceElement.addEventListener('click', (event) => {
-        console.log(event.currentTarget.children);
-
-    const children = event.currentTarget.children;
-
-    const hiddenParagraph = children[1];
-    // console.log(hiddenParagraph);
-
-    hiddenParagraph.classList.toggle('hidden')
-    })
-   
-}) 
-
-
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
